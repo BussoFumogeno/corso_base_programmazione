93603fc8b53badb163f990fd3f2b891c3b905b58
BussoFumogeno <s-bussolini.matteo@isiskeynes.it>
2022-10-12 21:45:11 +0200

    [ADD]creato il sesto programma

---
+++ 6_somma_numeri.py
@@ -0,0 +1,33 @@
+As = input("scegli il primo numero: ")
+Bs = input("scegli il secondo numero: ")
+Cs = input("scegli il terzo numero: ")
+
+A = int(As)
+B = int(Bs)
+C = int(Cs)
+
+D = 0
+
+if (A > B):
+    D = A
+else:
+    D = B
+
+if (A > C):
+    D = A
+else: 
+    D = C
+
+if (B > C):
+    D = B 
+else:
+    D = C
+
+if (A + B + C > 10):
+    print(D)
+else:
+    print(A + B + C)
+
+
+
+
