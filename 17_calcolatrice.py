795a3377c13f74e8d715c6e8f24e6abfa53bc144
BussoFumogeno <s-bussolini.matteo@isiskeynes.it>
2022-10-26 14:02:25 +0200

    [ADD]svolto l'esercizio numero 17

---
+++ 17_calcolatrice.py
@@ -0,0 +1,22 @@
+a = int(input("inserisci numero: "))
+b = int(input("inserisci numero: "))
+c = int(input("inserisci numero: "))
+
+if (c==1):
+    print (a+b)
+
+if (c==2):
+    print (a-b)
+
+if (c==3):
+    print (a/b)
+    if (b==0):
+        print ("errore")
+        if (a==0):
+            print ("errore")
+
+if (c==4):
+    print (a*b)
+
+if (c==5):
+    print (a+b)
