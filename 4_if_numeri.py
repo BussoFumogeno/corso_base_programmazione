553b12de17df21fd4580fea1c78028be0889b71d
BussoFumogeno <s-bussolini.matteo@isiskeynes.it>
2022-11-02 20:54:30 +0100

    [ADD]svolto l'esercizio numero 4

---
+++ 4_if_numeri.py
@@ -0,0 +1,11 @@
+a = int(input("inserisci un numero: "))
+b = int(input("inserisci un numero: "))
+
+if (a > 10):
+    print("FUORI SCALA")
+
+if (b > 50):
+    print("TROPPO GRANDE")
+
+if (a < 10 and b < 50):
+    print("TUTTO OK")
