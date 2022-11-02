56a363963894025abaf36d98e82b8a560a55ec90
BussoFumogeno <s-bussolini.matteo@isiskeynes.it>
2022-11-02 20:47:15 +0100

    [ADD]svolto l'esercizio numero 3

---
+++ 3_stampa_nomi.py
@@ -0,0 +1,19 @@
+lettera = (input("inserisci una lettera: "))
+if (lettera == "A"):
+    print("Alina")
+
+if (lettera == "B"):
+    print("Bruno")
+
+if (lettera == "C"):
+    print("Carlo")
+
+if (lettera == "D"):
+    print("Dario")
+
+if (lettera == "E"):
+    print("Emilio")
+
+if (lettera != "A" and lettera != "B" and lettera != "C" and lettera != "D" and lettera != "E"):
+    print("you choose...poorly")
+
